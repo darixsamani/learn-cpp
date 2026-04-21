707820b527d6433cf0358780d8fc5f0d81e5826b
Darix SAMANI SIEWE <samanidarix@gmail.com>
2026-04-22 00:20:32 +0100

    add currency project

---
+++ currency.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  double pesos;
+  double reais;
+  double soles;
+  double dollars;
+  std::cout << "Enter number of Colombian Pesos:\n";
+  std::cin >> pesos;
+  std::cout << "Enter number of Brazilian Reais:\n";
+  std::cin >> reais;
+  std::cout << "Enter number of Peruvian Soles:\n";
+  std::cin >> soles;
+  dollars = (0.00027*pesos + 0.20*reais + 0.29*soles);
+  std::cout << "US Dollars = $" << dollars << "\n";
+
+
+  
+  
+}
