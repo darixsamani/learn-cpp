f8c8ff0a5b9a77e8b4bf983618185252d3527a84
Darix SAMANI SIEWE <samanidarix@gmail.com>
2026-04-21 23:34:11 +0100

    add initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    // i don't like java
+    std::cout << " SSS   L     \n";
+    std::cout << "S   S  L     \n";
+    std::cout << "S      L     \n";
+    std::cout << " SSS   L     \n";
+    std::cout << "    S  L     \n";
+    std::cout << "S   S  L     \n";
+    std::cout << " SSS   LLLLL \n";
+    return 0;
+}
