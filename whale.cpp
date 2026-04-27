81386e31e575402591b06eeb4f5fb410d34f06ef
Darix SAMANI SIEWE <samanidarix@gmail.com>
2026-04-27 15:51:36 +0100

    add whale project

---
+++ whale.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+  // Whale, whale, whale.
+  // What have we got here?
+  std::string input = "turpentine and turtles";
+  std::vector<char> vowels = {'a','e', 'i', 'o', 'u'};
+  std::vector<char> result ;
+  for (int i=0; i<input.size(); i++){
+    for (int j=0; j< vowels.size(); j++){
+      if (input[i] == vowels[j]){
+        result.push_back(input[i]);
+        break;
+      }
+    }
+    if (input[i] == 'e' || input[i]== 'u'){
+        result.push_back(input[i]);
+      }
+  }
+  for (int i=0; i<result.size(); i++){
+    std::cout << result[i];
+  }
+  std::cout <<"\n";
+}
